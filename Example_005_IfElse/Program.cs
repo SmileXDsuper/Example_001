764bc6752a70d4446a56d2837ab72867024a8088
Shaten <azazshaten@gmail.com>
2022-11-25 09:47:16 +0600

    Example 005 If Else

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите Ваше имя: ");
+
+string username = Console.ReadLine();
+
+if("username.Tolower() == Маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
